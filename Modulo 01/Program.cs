da681358c5a862fd2d334254e05c5acebdbb6d02
Aldair Alvites <aldaofi100801@gmail.com>
2023-05-28 00:11:05 -0500

    actualización del módulo 1

---
+++ Modulo 01/Program.cs
@@ -0,0 +1,20 @@
+﻿//Primera vez escribiendo código en C#, módulo1
+Console.WriteLine("Hello, World!");
+
+//C# es un lenguaje sensitive case, es decir, las mayúsculas y minúsculas las detectará como carácteres diferentes
+//console.WriteLine("Hello world");
+
+//Lo mismo pasa si se envuelve en comillas simples, nos dará un error por exceso de caracteres en cadena literal
+//Console.WriteLine('Hello world')
+
+//Console.Write => hace que todo el contenido, independientemente de las líneas en las que se escriban, se mostrarán en consola de manera seguida
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code.");
+
+//Combinación de los dos tipos de método ejecutados
+Console.WriteLine("This is the first line.");
+
+Console.Write("This is ");
+Console.Write("the second ");
+Console.Write("line.");
