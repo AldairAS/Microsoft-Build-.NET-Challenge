1f860dab04d56019a30d641e0a0d3c4e634bd79f
Aldair Alvites <aldaofi100801@gmail.com>
2023-06-05 22:41:33 -0500

    Módulo 7

---
+++ Modulo 07/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//En este caso en el ejercicio se Utiliza try.NET que es una forma de ejecutar código sin necesidad de instalar naad de manera local.
